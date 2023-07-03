e52aa6a021b6bca95b58aca6176ba54a1dbd6db9
ben <benrobertson9876@gmail.com>
2023-07-03 17:47:04 +1000

    Completed script

--- check_data.rb
+++ check_data.rb
@@ -1,13 +1,17 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/ruby
 
+#!/usr/local/bin/ruby
 require 'yaml'
 
 # Load yaml
 data = YAML.load(File.read("./common.yaml"))
 
 expected_keys = ['patch_groups_as_a_hash','patching_options_as_a_hash']
-data.keys.each | keys |  {
-    if expected_keys.include?(keys)
-        puts "found key #{keys}"
-    end
-}
+data.keys.each do | keys |
+  if expected_keys.include?(keys)
+    puts "found key #{keys}"
+  else
+    puts "Found invalid key #{keys}. Failing job."
+    exit 1
+  end
+end
