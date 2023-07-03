c990407fec770ed6a59cd502124f593f94156458
ben <benrobertson9876@gmail.com>
2023-07-03 17:42:08 +1000

    fix

--- check_data.rb
+++ check_data.rb
@@ -6,8 +6,8 @@ require 'yaml'
 data = YAML.load(File.read("./common.yaml"))
 
 expected_keys = ['patch_groups_as_a_hash','patching_options_as_a_hash']
-data.keys.each | keys |  do
+data.keys.each | keys |  {
     if expected_keys.include?(keys)
         puts "found key #{keys}"
     end
-end
+}
